22facc7a9f87618f0cf4172c90cca4a5043fbda4
Duncan Jones <duncan.da.jones@gmail.com>
2025-07-15 18:47:26 +0100

    Added logging and started on lightning specifics

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -5,7 +5,7 @@ use lightning_tracker::web_socket::{WebSocketClient, WebSocketResult};
 
 fn test_main() -> WebSocketResult<()> {
     let request_url = Url::parse("https://www.blitzortung.org/en/live_lightning_maps.php")?;
-    let mut state_machine = WebSocketClient::new("wss://ws7.blitzortung.org/")?;
+    let state_machine = WebSocketClient::new("wss://ws7.blitzortung.org/")?;
     let requirements = state_machine.detect_requirements(Some(&request_url));
     match requirements {
         Ok(r) => {
